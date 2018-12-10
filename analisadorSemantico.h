a123b782fe8bb3d4ff197685c4157c2fa7ee7abf
RaelCappra <rael.cappra@gmail.com>
2018-12-09 22:04:16 -0200

    finished variable declarations in analisadorSemantico.h

--- analisadorSemantico.h
+++ analisadorSemantico.h
@@ -3,4 +3,11 @@
 #include "ResultType.h"
 class Variable;
 void print_errors(ResultType);
+void build_scopes(string);
+void find_calls(string);
+void find_operations(string);
+
+ResultType verify_assignments();
+ResultType verify_duplicates();
+ResultType verify_scopes();
 #endif
